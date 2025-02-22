cfa88f4bcbef468f44589a2ccf4f3effc9f42093
DHARUN RAJESH M <87368873+dharunrajesh@users.noreply.github.com>
2025-02-22 13:17:19 +0530

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,65 @@
+const cost = [500,200,17.6];
+const input = document.getElementById("input");
+const current = document.getElementById("curr");
+const totalv = document.getElementById("totv");
+const tax = document.getElementById("tax");
+const totalInvoice = document.getElementById("total");
+const generate = document.getElementById("generate");
+const qr = document.getElementsByClassName("qr-code");
+const download = document.getElementById("download");
+
+const service = ['Current Challan', 'Total Violation', 'TAX'];
+let listItems = "";
+let total = 0;
+let count1 = 0;
+let count2 = 0;
+let count3 = 0;
+
+
+
+function render(num) {
+        listItems += `
+            <tr class="row-data">
+              <td colspan="2">${service[num]} </td>
+              <td>₹${cost[num]}</td>
+            </tr>
+            `
+        totalInvoice.innerHTML = `₹${total}`;
+    // input.innerHTML = listItems;
+}
+
+generate.addEventListener("click", function() {
+  let url = 'https://api.qrserver.com/v1/create-qr-code/?data=₹' + total + '&amp;size=250x250';
+  location.replace(url)
+})
+
+current.addEventListener("click", function() {
+  if(count1 == 0){
+    total += 500;
+    sessionStorage.setItem("totals", total);
+    render(0);
+  input.innerHTML = listItems;}
+  count1 += 1;
+})
+
+totalv.addEventListener("click", function() {
+  if(count2 == 0){
+    total += 200;
+    sessionStorage.setItem("totals", total);
+    render(1);
+  input.innerHTML = listItems;}
+  count2 += 1;
+})
+
+tax.addEventListener("click", function() {
+  if(count3 == 0){
+    total += 17.6;
+    sessionStorage.setItem("totals", total);
+    render(2);
+  input.innerHTML = listItems;}
+  count3 += 1;
+})
+
+download.addEventListener("click", function() {
+  window.print();
+});
